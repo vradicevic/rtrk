8c5595c6f8634889e8c45ca0461ee3fcf4ec7c4b
vradicevic <valentin.radicevic@gmail.com>
2021-03-15 10:22:59 +0100

    nwst

--- drawVectors.py
+++ drawVectors.py
@@ -11,15 +11,15 @@ frame= 3
 blockSize = 32
 steps=[15,25,35,45,55,65,75,85,95]
 #pathVectors = "D:\\Visual Studio Code\\rtrk\\vectors\\carAwayEBMAblock32.bin"
-pathVectors = "C:\\vektori\\vectors0.bin"
+pathVectors = "H:\\vektori\\vectors0.bin"
 vectorsFile = open(pathVectors,"r+")
 buffer = np.fromfile(vectorsFile,dtype=np.int16)
 vectors = []
-numOfVs = buffer[0]
+numOfVs = int(len(buffer)/6)
 print(numOfVs)
 print("a iz lena")
-print(len(buffer))
-for i in range(1,len(buffer),6):
+
+for i in range(0,len(buffer),6):
     vectors.append((buffer[i:i+6])) 
     #svaki vektor je složen od 5 integera na sljedeći način u bin-u
     # ::from-[0]=x i [1]=y   2 integera jedan x drugi y 
@@ -27,30 +27,29 @@ for i in range(1,len(buffer),6):
     # ::length- [4] 1 integer udaljenosti from i to točke
     # ::angle- [5] 1 integer kut vektora
 vectorsFile.close()
-# pathImage = "D:\\auticNew\\autic328.png"
-# saveImagePath = "D:\\results\\auticClosingAllBlocks\\carawayblackBlock32Step35TSS.png"
+pathImage = "H:\\frame1.png"
+#saveImagePath = "D:\\results\\auticClosingAllBlocks\\carawayblackBlock32Step35TSS.png"
 
 
-# # file = open(pathImage,'rb')
+# file = open(pathImage,'rb')
 
-# # yuv = np.frombuffer(file.read(width*height*3), dtype=np.uint8).reshape(height,width,3)
-# # file.close()
-# # png = cv2.cvtColor(yuv,cv2.COLOR_YUV2BGR)
-# png = cv2.imread(pathImage)
+# yuv = np.frombuffer(file.read(width*height*3), dtype=np.uint8).reshape(height,width,3)
+# file.close()
+# png = cv2.cvtColor(yuv,cv2.COLOR_YUV2BGR)
+png = cv2.imread(pathImage)
 
-# w_segments = int(width/blockSize)
-# h_segments = int(height/blockSize)
-# vectorsCount = w_segments*h_segments
-# numofVectors = 0
-# for bcount in range(0, vectorsCount, 1):
+
+ 
+numofVectors = 0
+for bcount in range(0, numOfVs, 1):
         
-#         if (vectors[bcount][0],vectors[bcount][1]) != (vectors[bcount][2],vectors[bcount][3]) :
-#             numofVectors+=1
-#             pointFrom = vectors[bcount][0:2]
-#             pointTo = vectors[bcount][2:4]
-#             print("Length: "+ str(vectors[bcount][4]))
-#             cv2.arrowedLine(png,(pointFrom[0],pointFrom[1]), (pointTo[0],pointTo[1]),(0,255,0), 1)
+        if (vectors[bcount][0],vectors[bcount][1]) != (vectors[bcount][2],vectors[bcount][3]) :
+            
+            pointFrom = vectors[bcount][0:2]
+            pointTo = vectors[bcount][2:4]
+            print("Length: "+ str(vectors[bcount][5]))
+            cv2.arrowedLine(png,(pointFrom[0],pointFrom[1]), (pointTo[0],pointTo[1]),(0,255,0), 1)
 
-# cv2.imshow("Framnjo",png)
+cv2.imshow("Framnjo",png)
 
-# cv2.waitKey()
+cv2.waitKey()
