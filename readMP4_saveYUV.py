a4cd81a45be3ab1cb92c972ecfe6ae081b7141fe
vradicevic <valentin.radicevic@gmail.com>
2021-03-17 13:10:57 +0100

    read mp4 write yuv444

--- readMP4_saveYUV.py
+++ readMP4_saveYUV.py
@@ -2,23 +2,29 @@ import framecollect as fc
 from cv2 import cv2
 import io
 
-videopath = "D:\\Videosekvence\\mirna_right720.mp4"
-savepath = "D:\\Videosekvence\\YUV\\test.yuv"
-video = cv2.VideoCapture(videopath)
-file = open(savepath,'wb')
+videopath = "D:\\Videosekvence\\moving_dashboard720.mp4"
+savepath = "D:\\Videosekvence\\YUV\\moving_dashboardYUV444.yuv"
+
+
+file = open(savepath,'ab')
 
 ok = True
 f= io.BytesIO()
 cnt=0
-ok,frame = fc.read_frame(video,int(94.5*60))
-
-while cnt<120:
+video = cv2.VideoCapture(videopath)
+for i in range(0,24120,60):
+    ok,frame = fc.read_frame(video,60)
     if ok:
-        cnt+=1
         yuv = cv2.cvtColor(frame,cv2.COLOR_BGR2YUV)
-        f.write(yuv.tobytes())
+        file.write(yuv.tobytes())
         ok,frame = fc.read_frame(video,0)
-    else:
+        if ok:
+            yuv = cv2.cvtColor(frame,cv2.COLOR_BGR2YUV)
+            file.write(yuv.tobytes())
+        else:
+            break
+    else :
         break
+    
 
-file.write(f.getbuffer())
+file.close()
