fd29bf6ee38d43de8183d54a171a8d3611f0d898
vradicevic <valentin.radicevic@gmail.com>
2021-07-12 18:53:53 +0200

    sth

--- script.py
+++ script.py
@@ -1,23 +1,26 @@
-import framecollect as fc
-import new_tss as tss
+
 from cv2 import cv2
 import io
-videopath = "D:\\auticClosing.mp4"
-imagespath = "D:\\auticClosing"
+videopath = "D:\\VISION_SDK_02_12_01_00\\vision_sdk\\tools\\network_tools\\bin\\Videosekvence\\dummyNV12.yuv"
+imagespath = "D:\\VISION_SDK_02_12_01_00\\vision_sdk\\tools\\network_tools\\bin\\Videosekvence\\singledummyNV12.yuv"
 
 
-#fc.getFrames(videopath,0,500,imagespath,name="autic")
+fileRead = open(videopath,"rb")
+fileWrite = open(imagespath,'ab')
+frameSize = int(1280*720*3/2)
+ok = True
 
+cnt=0
+fileRead.seek(0*frameSize,0)
+binary = fileRead.read(frameSize)
+fileWrite.write(binary)
+fileRead.seek((1)*frameSize,0)
+binary = fileRead.read(frameSize)
+fileWrite.write(binary)
 
-
-filePath = imagespath+ "\\auticALL.yuv"
-f= io.BytesIO()
-file = open(filePath,'wb')
-
-for num in range(1,15,1):
-    frame = cv2.imread(imagespath+"\\autic"+str(num)+".png") 
-    yuv = cv2.cvtColor(frame,cv2.COLOR_BGR2YUV)
-    f.write(yuv.tobytes())
     
-file.write(f.getbuffer())
+
+fileRead.close()
+fileWrite.close()
+
     
